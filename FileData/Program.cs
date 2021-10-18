1cdc0234075abe7cc03e2f22225059c2e0d78891
MahendraAwate <mahendra33.awate@gmail.com>
2021-10-18 18:57:20 +0530

    Add project files.

---
+++ FileData/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            FileDetails details = new FileDetails();
+            string value = details.Version(args[1]);
+            string version = (args[0].Contains("-v") || args[0].Contains("--v") || (args[0]).Contains("/v")) ? details.Version(args[1]) : string.Empty;
+            int size = (args[0].Contains("-s") || args[0].Contains("--s") || (args[0]).Contains("/s")) ? details.Size(args[1]) : 0;
+            Console.WriteLine("Responce is:"+ value);            
+        }
+    }
+}
